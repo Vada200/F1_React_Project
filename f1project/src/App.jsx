9982c2da3013cf74972520be4efbe299d56c70c1
Vada <vadahiv2019@gmail.com>
2025-12-11 20:34:15 +0100

    Creating the rest of the current team cards

--- f1project/src/App.jsx
+++ f1project/src/App.jsx
@@ -11,7 +11,21 @@ function App() {
     <>
           <h1 className='main-title'>2025 F1 Constructors</h1>
 
+      <div className='teams'>
           <TeamContainer image={TEAM_NAMES_LOGOS[0].image} name={TEAM_NAMES_LOGOS[0].name} onSelect={() => console.log("Kiválasztott csapat:", TEAM_NAMES_LOGOS[0].name)}/>
+          <TeamContainer {...TEAM_NAMES_LOGOS[1]} onSelect={() => console.log("Kiválasztott csapat:", TEAM_NAMES_LOGOS[0].name)}/>
+          <TeamContainer {...TEAM_NAMES_LOGOS[2]} onSelect={() => console.log("Kiválasztott csapat:", TEAM_NAMES_LOGOS[0].name)}/>
+          <TeamContainer {...TEAM_NAMES_LOGOS[3]} onSelect={() => console.log("Kiválasztott csapat:", TEAM_NAMES_LOGOS[0].name)}/>
+          <TeamContainer {...TEAM_NAMES_LOGOS[4]} onSelect={() => console.log("Kiválasztott csapat:", TEAM_NAMES_LOGOS[0].name)}/>
+          <TeamContainer {...TEAM_NAMES_LOGOS[5]} onSelect={() => console.log("Kiválasztott csapat:", TEAM_NAMES_LOGOS[0].name)}/>
+          <TeamContainer {...TEAM_NAMES_LOGOS[6]} onSelect={() => console.log("Kiválasztott csapat:", TEAM_NAMES_LOGOS[0].name)}/>
+          <TeamContainer {...TEAM_NAMES_LOGOS[7]} onSelect={() => console.log("Kiválasztott csapat:", TEAM_NAMES_LOGOS[0].name)}/>
+          <TeamContainer {...TEAM_NAMES_LOGOS[8]} onSelect={() => console.log("Kiválasztott csapat:", TEAM_NAMES_LOGOS[0].name)}/>
+          <TeamContainer {...TEAM_NAMES_LOGOS[9]} onSelect={() => console.log("Kiválasztott csapat:", TEAM_NAMES_LOGOS[0].name)}/>
+      </div>
+
+
+
     </>
   )
 }
